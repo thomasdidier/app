4fd1cd8cb69f8a40ecf3f2441184d1033cceb8dd
Unknown <thomasdidier@outlook.fr>
2018-04-25 16:59:54 +0200

    modif css et js

--- ressources/js/acceuil-script.js
+++ ressources/js/acceuil-script.js
@@ -9,13 +9,7 @@ var user = {id: "toto", password: "123"}
 
 
 
-////////////////////////////////////////////////////////////////////
-/**** variable animation ****/
 
-var btn = document.getElementById('btn')
-	,animated = document.getElementById('rotating')
-	,removeBtn = document.getElementById('removeBtn')
-	,btn2 = document.getElementById('btn2')
 
 ////////////////////////////////////////////////////////////////////
 /**** connexion ****/
@@ -26,20 +20,40 @@ var btn = document.getElementById('btn')
 var log = connexion.addEventListener("click", function(){
 	if (inputId.value == "toto" && inputPassword.value == "123") {
 		document.location.href="ressources/html/acceuil-connect.html"
+		localStorage.setItem(inputId.value, inputPassword.value);
+		alert(localStorage.getItem(inputId.value, inputPassword.value));
 	}else{
 		document.getElementById("bonjour").innerHTML = "<h1>password or userName are wrong </h1>"
 	}
 })
 
 
-////////////////////////////////////////////////////////////////////
-/**** animation ****/
+//////////////////////////////////////////////////////////////////////////////////
+/**** horloge ****/
 
-btn.addEventListener("click", function(event){
-		animated.id = "rotating";
-				animated.className = "animated bounce displayBtn";
-						btn.innerHTML = '<button type="button" id="btn2" >remove class</button>'
+setInterval(function() {
+	var currentTime = new Date();
+	var hours = currentTime.getHours();
+	var minutes = currentTime.getMinutes();
+	var seconds = currentTime.getSeconds();
 
-})
+if (hours >= 24){
+hours = -24;
+}
+	
+	if(seconds < 10){
+		seconds = "0" + seconds;
+	}
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+
+	var clockTime = hours + ":" + minutes;
+	var clock = document.querySelector(".clock").innerText = clockTime
+	
+ }, 1000);
+
+
+
 
 
--- ressources/js/connect-script.js
+++ ressources/js/connect-script.js
@@ -7,30 +7,14 @@ var user = {id: "toto", password: "123"}
 	,connexion = document.getElementById("connexion")
 
 
-////////////////////////////////////////////////////////////////////
-/**** variable animation ****/
-
-var btn = document.getElementById('btn')
-	,animated = document.getElementById('rotating')
-	,removeBtn = document.getElementById('removeBtn')
-	,btn2 = document.getElementById('btn2')
 
 
-////////////////////////////////////////////////////////////////////
-/**** animation ****/
 
-btn.addEventListener("click", function(event){
-		animated.id = "rotating";
-				animated.className = "animated bounce displayBtn";
-						btn.innerHTML = '<button type="button" id="btn2" >remove class</button>'
 
-})
-
-document.getElementById("connecter").innerHTML = "<h2>salut toto</h2>"
 
 
 //////////////////////////////////////////////////////////////////////////////////
-/**** meteo ****/
+/**** current meteo ****/
 
 $(document).ready(function(){
 	$('#submitWeather').click(function(){
@@ -47,6 +31,7 @@ $(document).ready(function(){
 
 				$("#show").html(widget);
 				$("#city").val('');
+				document.getElementById('weather').id = 'weatherAfter'
 			}
 		});
 	
@@ -60,7 +45,7 @@ $(document).ready(function(){
 
 function show(data){
 
-	return"<h3><img src='http://openweathermap.org/img/w/"+data.weather[0].icon+".png'></h3>" +
+	return"<div><h3><img src='http://openweathermap.org/img/w/"+data.weather[0].icon+".png'></h3>" +
 		"<h3><strong>description</strong>:"+ data.weather[0].description +"</h3>" +
 		"<h3><strong>Meteo a</strong>:"+ " " + data.name + ", " + data.sys.country +" </h3>" +
 		"<h3><strong>temperature</strong>:"+ " " + data.main.temp + " °" +"</h3>" +
@@ -68,6 +53,148 @@ function show(data){
 		"<h3><strong>temperature max</strong>:"+ " " + data.main.temp_max + " °" +"</h3>" +
 		"<h3><strong>Vent</strong>:"+ " " + data.wind.speed +"</h3>" + 
 		"<h3><strong>humidity</strong>:"+ " " + data.main.humidity + " %" +"</h3>" +
-		"<h3><strong>pression</strong>:"+ " " + data.main.pressure +"</h3>";
+		"<h3><strong>pression</strong>:"+ " " + data.main.pressure +"</h3> </div>";
 		;
 }
+
+
+
+//////////////////////////////////////////////////////////////////////////////////
+/**** forecast meteo ****/
+
+
+
+
+
+
+$(document).ready(function(){
+	$('#submitWeather').click(function(){
+	var city = $("#city").val();
+	
+	if(city != "" ){
+		
+
+
+
+		$.ajax({
+			url:'https://api.openweathermap.org/data/2.5/forecast/?q='+ city +',fr&units=metric&lang=fr&cnt=7&appid=06a821a408fbe8ad9d8881394efee483',
+			type: "GET",
+			dataType:"jsonp",
+			success: function(data){
+
+				var demain = data.list[0].main.temp
+				,apresDemain = data.list[1].main.temp
+				,dansTroisJour = data.list[2].main.temp
+				,dansQuatreJour = data.list[3].main.temp
+				,demainPluit = data.list[0].main.humidity
+				,apresDemainPluit = data.list[1].main.humidity
+				,dansTroisJourPluit = data.list[2].main.humidity
+				,dansQuatreJourPluit = data.list[3].main.humidity
+				
+
+
+
+				
+var line = document.getElementById("line")
+var lineChart = new Chart(line, {
+	type:"line",
+	data: {
+		labels:['Demain', 'Apres demain', 'Dans 3 jour', 'Dans 4 jour'],
+		datasets: [
+			{
+				label: 'Temperature (en °)',
+				fill: true,
+				lineTension: 0.1,
+				backgroundColor: "rgba(75,192,192,0.4)",
+				borderColor: "rgba(75,192,192,1)",
+				borderCapStyle:"butt",
+				borderDash: [],
+				borderDashOffset:0.0,
+				borderJoinStyle: 'miter',
+				pointBordercolor: 'rgba(75,192,192.1)',
+				pointBackgroundColor:"#fff",
+				pointBorderWidth: 1,
+				pointHoverRadius: 5,
+				pointHoverBackgroundColor:"rgba(75,192,192,1)",
+				pointHoverBorderColor: "rgba(220,220,220,1)",
+				pointHoverBorderWidth: 2,
+				pointRadius: 1,
+				pointHitRadius: 10,
+				data:[demain ,apresDemain ,dansTroisJour ,dansQuatreJour],
+
+
+
+			},
+						{
+				label: 'humidité (en %)',
+				fill: true,
+				lineTension: 0.1,
+				backgroundColor: "rgba(75,192,192,0.4)",
+				borderColor: "rgba(75,192,192,1)",
+				borderCapStyle:"butt",
+				borderDash: [],
+				borderDashOffset:0.0,
+				borderJoinStyle: 'miter',
+				pointBordercolor: 'rgba(75,192,192.1)',
+				pointBackgroundColor:"#fff",
+				pointBorderWidth: 1,
+				pointHoverRadius: 5,
+				pointHoverBackgroundColor:"rgba(75,192,192,1)",
+				pointHoverBorderColor: "rgba(220,220,220,1)",
+				pointHoverBorderWidth: 2,
+				pointRadius: 1,
+				pointHitRadius: 10,
+				data:[demainPluit ,apresDemainPluit ,dansTroisJourPluit ,dansQuatreJourPluit],
+
+
+
+			}
+		]
+	},
+	options: {
+		scales: {
+			yAxes: [{
+				ticks: {
+					beginAtZero: true
+				}
+			}]
+		}
+	}
+});
+				console.log(data)
+			}
+		});
+	
+	}else{
+		$("#error").html('Field connot be empty');
+	}
+
+	});
+	
+});
+
+
+//////////////////////////////////////////////////////////////////////////////////
+/**** horloge ****/
+
+setInterval(function() {
+	var currentTime = new Date();
+	var hours = currentTime.getHours();
+	var minutes = currentTime.getMinutes();
+	var seconds = currentTime.getSeconds();
+
+if (hours >= 24){
+hours = -24;
+}
+	
+	if(seconds < 10){
+		seconds = "0" + seconds;
+	}
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+
+	var clockTime = hours + ":" + minutes + ":" + seconds;
+	var clock = document.getElementById("clock");
+	clock.innerText = clockTime;
+ }, 1000);
